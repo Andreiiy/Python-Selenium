76385a403c5851259ed2f60e81259fb39520fc40
Andrei <41228222+Andreiiy@users.noreply.github.com>
2020-01-16 11:04:25 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,16 @@
+#region---------------------------------I M P O R T S----------------------------------------
+from TestFacebook import TestFacebook
+#endregion
+
+
+
+
+
+#region------------  M A I N  ----------------------------
+def main():
+    testFacebook = TestFacebook("Chrom")
+    testFacebook.startTest()
+#endregion
+
+if __name__=="__main__":
+    main()
